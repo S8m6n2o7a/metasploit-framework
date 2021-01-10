0480cb8fab83a0ee50127fec91f377efd47ac9c2
S8m6n2o7a <64481672+S8m6n2o7a@users.noreply.github.com>
2021-01-10 13:59:56 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 ##Metasploit install
+echo "
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    |M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+            +-+-+ +-+-+-+-+-+-+-+-+-+
+            |b|y| |S|8|m|6|n|2|o|7|a|
+            +-+-+ +-+-+-+-+-+-+-+-+-+
+"
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
   ./msfinstall
